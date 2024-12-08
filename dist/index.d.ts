22cca8c0a0e1281bf3e5be745d4577c9ff9253bb
SNKK62 <koki.seno62@gmail.com>
2024-12-08 21:54:04 +0900

    add dist

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+import { LRLanguage, LanguageSupport } from "@codemirror/language";
+declare const KOVRayLanguage: LRLanguage;
+declare function KOVRay(): LanguageSupport;
+export { KOVRayLanguage, KOVRay };
